963302e533a73ca1bbaad671eebce905fe167312
Atif ahmed <167696985+Atif1254@users.noreply.github.com>
2025-02-07 09:37:18 +0000

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+
+const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
+
+export const metadata: Metadata = {
+  title: "Ecommerce Website",
+  description: "Ecommerce Website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+
+        {/* Snipcart API Script */}
+      
+      </body>
+    </html>
+  );
+}
